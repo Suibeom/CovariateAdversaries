3af843c7b0d9aa4c6747b2330d1eec619fbcb619
Ubuntu <ubuntu@ip-172-31-29-219.us-west-2.compute.internal>
2018-12-02 21:27:06 +0000

    New from the cloud!

--- mlp.jl
+++ mlp.jl
@@ -1,6 +1,6 @@
 using Flux, Flux.Data.MNIST, Statistics
 using Flux: onehotbatch, onecold,mse, crossentropy, throttle
-using Base.Iterators: repeated
+using Base.Iterators: repeated, partition
 using Juno: @progress
 
 using CuArrays
@@ -8,13 +8,10 @@ using CuArrays
 # Classify MNIST digits with a simple multi-layer-perceptron
 
 imgs = MNIST.images()[1:1000]
-# Stack images into one large batch
 X = hcat(float.(reshape.(imgs, :))...) |> gpu
-
 labels = MNIST.labels()[1:1000]
-# One-hot-encode the labels
 Y = onehotbatch(mod.(labels,2), 0:1) |> gpu
-N=32
+N=64
 encoder = Dense(28^2, N, leakyrelu) |> gpu
 decoder = Dense(N, 28^2, leakyrelu) |> gpu
 
@@ -32,20 +29,18 @@ function nansafemin(a,b) # First should be bounded!!
 end
 
 m = Chain(
-  Dense(28^2, 32, relu),
-  Dense(32, 2),
+  Dense(28^2, N, relu),
+  Dense(N, 2),
   softmax) |> gpu
 #clippedloss(x, y) = nansafemin(mse(m(a(x)), y),crossentropy(m(a(x)),y))
 #loss(x, y) = min(clippedloss(x, y),clippedloss(x, -1*y.+1))
-loss(x, y) = min(mse(m(a(x)), y),mse(m(a(x)), -1*y.+1)) + 0.01*min(mse(m(x)), y),mse(m(x), -1*y.+1))
-ac(x, y) = mean(onecold(m(a(x))) .== onecold(y))
-accuracy(x,y) = max(ac(x,y), 1-ac(x,y))
-dataset = repeated((X, Y), 2)
-dataseta = repeated((X,Y),2)
+
 
 opt1 = ADAM(params(a))
-opt2 = ADAM(params(m))
 
+loss(x, y) = min(mse(m(a(x)), y),mse(m(a(x)), -1*y.+1)) + 0.1*min(mse(m(x), y),mse(m(x), -1*y.+1))
+ac(x, y) = mean(onecold(m(a(x))) .== onecold(y))
+accuracy(x,y) = max(ac(x,y), 1-ac(x,y))
 diffloss(x, y) = aloss(x,y) - 2*loss(x,y)
 function evalcb()
    @show(accuracy(X, Y))
@@ -66,29 +61,44 @@ function sample()
   # Stack them all together
   hcat(vcat.(before, after)...)
 end
+imgss = MNIST.images()
+labelss=MNIST.labels()
+@progress for k in 1:20000
 
-#println("Pretraining adversary")
-#Flux.train!(pretrainloss, dataseta, opt2, cb=throttle(advcb,10))
-#Flux.train!(aloss, dataset,opt2,cb=throttle(advcb,10))
+imgs = deepcopy([imgss[b + (1000*mod(k,60))] for b in 1:1000])
+X1 = hcat(float.(reshape.(imgs, :))...) |> gpu 
+labels = deepcopy([labelss[b + (1000*mod(k,60))] for b in 1:1000])
+Y1 = onehotbatch(mod.(labels,2), 0:1) |> gpu 
+dataset = repeated((X1,Y1),2)
+dataseta = dataset
 
-@progress for k in 1:2000
-diffloss(x, y) = aloss(x,y) - 100*loss(x,y)
+
+
+m = Chain(
+  Dense(28^2, 32, relu),
+  Dense(32, 2),
+  softmax) |> gpu
+println("Pretraining adversary")
+opt2 = ADAM(params(m))
+for i in 1:100
+Flux.train!(pretrainloss, dataseta, opt2 )
+end
+diffloss(x, y) = aloss(x,y) - 1*(1+(k/20))*loss(x,y)
 @info "Epoch $k"
-for i = 1:20
+for i = 1:10
 println("Training autoencoder")
 Flux.train!(diffloss, dataset, opt1, cb = throttle(evalcb,10))
 println("Training adversary")
 A0 = advcb()
 
 #Flux.train!(loss, dataseta, opt2, cb=throttle(advcb,10))
-while A0 > A #Train until you get back to at least as good as you had!
-Flux.train!(loss, dataseta, opt2, cb=throttle(advcb,10))
-end
+#while A0 > A #Train until you get back to at least as good as you had!
+#Flux.train!(loss, dataseta, opt2, cb=throttle(advcb,10))
+#end
 #println("Breakpoint reached.")
-for i in 1:100
+for i in 1:10
 Flux.train!(loss, dataseta, opt2)
 end
-
-save("sample"*lpad(string(k),4,string(0))*".png", sample())
+GC.gc(); CuArrays.reclaim()
 end
 end
